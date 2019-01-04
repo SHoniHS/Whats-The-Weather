125ee3714ec7028547938fb24e69cd4963b5e404
SHS <SHS@macbook-pro-de-shs.local>
2019-01-05 00:10:50 +0100

    1st version

--- Whats The Weather/ViewController.swift
+++ Whats The Weather/ViewController.swift
@@ -10,11 +10,78 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var cityTextField: UITextField!
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+    
+    @IBAction func getWeather(_ sender: AnyObject) {
+        
+        let url = URL(string: "http://www.weather-forecast.com/locations/" + cityTextField.text! + "/forecasts/latest")!
+        
+        let request = NSMutableURLRequest(url: url)
+        
+        let task = URLSession.shared.dataTask(with: request as URLRequest){
+            data, response, error in
+            
+            var message = ""
+            
+            if error != nil {
+                
+                print (error as Any)
+                
+            } else {
+                
+                if let unwrappedData = data {
+                    
+                    let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                    
+                    var stringSeparator = "<p class=\"b-forecast__table-description-content\"><span class=\"phrase\">"
+                    
+                    if let contentArray = dataString?.components(separatedBy: stringSeparator) {
+                        
+                        if contentArray.count > 0 {
+                            
+                            stringSeparator = "</span>"
+                            
+                            let newContentArray = contentArray[3].components(separatedBy: stringSeparator)
+                            
+                            if newContentArray.count > 0 {
+                                
+                                message = newContentArray[0].replacingOccurrences(of: "&deg;", with: "º")
+                                
+                                print (message)
+                                
+                            }
+                            
+                        }
+                    }
+                }
+                
+                if message == "" {
+                    
+                    message = "The weather there could not be found. Please try again until you select London..."
+                    
+                }
+                
+                DispatchQueue.main.sync(execute: {
+                    
+                    self.resultLabel.text = message
+                    
+                })
+                
+            }
+        }
+        
+        task.resume()
+        
+        }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
+        
+        }
 
 }
-
